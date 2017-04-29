b6233af3b4ac2a74675e2c9b787d8eebce42ef47
vlw0052 <leon@vlw2.com>
2017-04-29 16:52:15 -0500

    Products Component Added

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,22 @@
+/*
+*	products.ts
+* 	Product Component is the where component about products will come from.
+*	It will display the following components:
+*	Add Product Component
+*	Add Product Component
+*	Search Product Component
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'products',
+	template: `	
+			<h2>Products Component</h2>
+			<router-outlet></router-outlet>
+			`,
+})
+
+export class ProductsComponent {
+	
+}
