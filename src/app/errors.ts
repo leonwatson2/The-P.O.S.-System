350e660e8e3e8246ad6b0b4b0f819f8207674560
vlw0052 <leon@vlw2.com>
2017-04-30 14:53:30 -0500

    Added Error Types
    AddItemErrors - for when adding, any kind of item
    EditItemErrors - same when editing

--- src/app/errors.ts
+++ src/app/errors.ts
@@ -8,3 +8,14 @@ export enum LoginErrors{
 	INVALID = 2,
 	SERVERERROR
 }
+
+//Adding <t> error
+export enum AddItemErrors{
+	INVALID = 2,
+	DUPLICATE
+}
+//Adding <t> error
+export enum EditItemErrors{
+	INVALID = 2,
+	NOTFOUND 
+}
