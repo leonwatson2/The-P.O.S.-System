248e230dc65d2959c2917093adb7be1b2f15a489
vlw0052 <leon@vlw2.com>
2017-05-04 18:15:45 -0500

    Videos 3 - 5

--- src/app/discounts/discount.form.component.ts
+++ src/app/discounts/discount.form.component.ts
@@ -4,29 +4,90 @@
 */
 
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { iDiscount, Discount } from '../classes';
 
 @Component({
 	selector:'discount-form',
 	template:`
-		<form>
+		<form [formGroup]="discountForm" (ngSubmit)="submitForm(discountForm.value)">
 			<label for="disName">Discount Name</label>
 			<input type="text" 
 					id="disName"
-					placeholder="PAPA50"/>
+					placeholder="PAPA50"
+					[formControl]="discountForm.controls['name']"/>
+
+
 			<label for="disValue">Discount Value</label>
 			<input type="number" 
 					id="disValue"
-					placeholder="PAPA50"/>
+					placeholder="PAPA50"
+					[formControl]="discountForm.controls['value']"/>
+
+
 
 			<label for="isPerc">Is Percentage</label>
-			<input type="checkbox" id="isPerc"/>
+			<input 
+					type="checkbox" 
+					id="isPerc"
+					[formControl]="discountForm.controls['isPercentage']"/>
 
 			<button type="submit">Add Discount</button>
-
+	
 		</form>
-	`
+		<div class="list-group">
+			<div class="list-group-item" *ngFor="let discount of discounts">
+				{{discount.id}}: {{discount.name}} - {{discount.value}}
+			</div>
+		</div>
+		
+	`,
+	styleUrls:['../solar-bootstrap-theme.min.css']
 })
 
 export class DiscountFormComponent{
+	discountForm:FormGroup;
+	discounts:Discount[] = [];
+	constructor(private fb:FormBuilder){}
+
+	ngOnInit(){
+		this.initForm();
+	}
+
+	initForm(){
+		this.discountForm = this.fb.group({
+			'name':[null, Validators.compose([Validators.required])],
+			'value':[null, Validators.compose([Validators.required])],
+			'isPercentage':true
+		});
+	}
+	submitForm(values:iDiscount){
+		//check if the discount already exist
+		if(!this.isDiscount(values))
+			this.discounts.push(this.createDiscount(values));
+		
+	}
+	//	createDiscount(discount:iDiscount)
+	//Create Discount returns a Discount object 
+	//	with a random id from the interface values passed in.
+	createDiscount(discount:iDiscount):Discount{
+		return new Discount(
+				Math.floor(Math.random()*10000000000),
+				discount.name,
+				discount.value,
+				discount.isPercentage
+			);
+	}
+
+	//	isDiscount(discount:iDiscount):Boolean
+	//Checks if a discount with the same name exist in discount array
+	//	return true or false, using findIndex
+	isDiscount(discount:iDiscount):Boolean{
+		let bool = this.discounts.findIndex((d)=>{
+			return d.name == discount.name
+		})
+		return bool >= 0;
+	}
+
 
 }
--- src/app/discounts/discount.module.ts
+++ src/app/discounts/discount.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AddDiscountComponent } from './add-discount.component';
 import { DiscountFormComponent } from './discount.form.component';
@@ -13,7 +14,9 @@ import { DiscountFormComponent } from './discount.form.component';
 	],
 	imports:[
 		CommonModule,
-		BrowserModule
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule
 	],
 	exports:[
 		AddDiscountComponent
