7ae64f55f1da4128b5c5bdb192f83831cd856ddf
vlw0052 <leon@vlw2.com>
2017-05-04 21:34:10 -0500

    Styles Added

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			options:{
+				style:'compressed',
+				sourcemap:'auto'
+			},
+			css: {
+				files: [{
+		        expand: true,
+		        cwd: 'src/app/styles/sass',
+		        src: '*.scss',
+		        dest: 'src/app/styles/',
+		        ext: '.css'
+		      }]
+			}
+		},
+		watch: {
+			css: {
+				files: 'src/app/styles/sass/*.scss',
+				tasks: ['sass', 'copy']
+			}
+		},
+		copy: {
+	        main: {
+	            files: [
+	                {
+	                	src: 'src/app/styles/style.css', 
+	                	dest:'src/style.css'
+	                }
+	            ]
+	       	}
+	    }
+    
+	});
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
